6f9b46b3fecc528f11487c706321721165f1577b
ZDroid <legospace9876@gmail.com>
2013-07-21 17:43:08 +0200

    Script fix

--- quickbuild.sh
+++ quickbuild.sh
@@ -1,15 +1,18 @@
+#!/bin/sh
+# Build Birdie quickly.
+
 # We need to make our build directory for all of our temp files.
 rm -r build
 mkdir build
 
-#Enter the build Directory
+# Enter the build Directory
 cd build
 
-#Now we initiate cmake in this dir
+# Now we initiate cmake in this dir
 cmake ..
 
-#Next we build the source files!
+# Next we build the source files!
 make
 
-#Next we copy the executable to our root project file.
+# Next we copy the executable to our root project file.
 cp ./src/birdie ../birdie
